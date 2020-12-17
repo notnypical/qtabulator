0be8d0d740d4d968a02e8921a526106e337e8f12
notnypical <46350569+notnypical@users.noreply.github.com>
2020-12-17 14:13:46 +0100

    Refactor 'Dialog Title Box' widget

--- dialog_title_box.cpp
+++ dialog_title_box.cpp
@@ -1,20 +1,20 @@
 /**
  * Copyright 2020 NotNypical, <https://notnypical.github.io>.
  *
- * This file is part of qTabulator.
+ * This file is part of Tabulator-Qt.
  *
- * qTabulator is free software: you can redistribute it and/or modify
+ * Tabulator-Qt is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * qTabulator is distributed in the hope that it will be useful,
+ * Tabulator-Qt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with qTabulator.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Tabulator-Qt.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "dialog_title_box.h"
@@ -25,31 +25,31 @@
 #include <QVBoxLayout>
 
 
-DialogTitleBox::DialogTitleBox(QWidget *parent) :
-    QWidget(parent)
+DialogTitleBox::DialogTitleBox(QWidget *parent)
+    : QWidget(parent)
 {
-    QLabel *name = new QLabel(QStringLiteral("<strong style=\"font-size:large;\">%1</strong> v%2").arg(QApplication::applicationName(), QApplication::applicationVersion()));
-    QLabel *description = new QLabel(QStringLiteral("A CSV editor written in Qt for C++."));
-
-    QWidget *widgetTmp = new QWidget;
-    QVBoxLayout *vboxlayoutTmp = new QVBoxLayout(widgetTmp);
-    const int vboxlayoutHeight = name->sizeHint().height() + vboxlayoutTmp->layout()->spacing() + description->sizeHint().height();
-
-    QSvgWidget *logo = new QSvgWidget;
+    auto *logo = new QSvgWidget;
     logo->load(QStringLiteral(":/icons/apps/22/tabulator.svg"));
-    logo->setFixedSize(vboxlayoutHeight, vboxlayoutHeight);
 
-    QVBoxLayout *labels = new QVBoxLayout;
+    auto *name = new QLabel(tr("<strong style=\"font-size:large;\">%1</strong> v%2").arg(QApplication::applicationName(), QApplication::applicationVersion()));
+    auto *description = new QLabel(tr("An editor tool for documents with character-separated values"));
+
+    auto *labels = new QVBoxLayout;
     labels->addWidget(name);
     labels->addWidget(description);
 
-    QHBoxLayout *titleBox = new QHBoxLayout;
-    titleBox->addWidget(logo);
-    titleBox->addLayout(labels);
-
     // Main layout
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addLayout(titleBox, 1);
+    m_layout = new QHBoxLayout(this);
+    m_layout->addWidget(logo);
+    m_layout->addLayout(labels);
 
-    setLayout(layout);
+    // Set logo size
+    const int height = name->sizeHint().height() + labels->layout()->spacing() + description->sizeHint().height();
+    logo->setFixedSize(height, height);
+}
+
+
+void DialogTitleBox::setZeroMargins()
+{
+    m_layout->setContentsMargins(0, 0, 0, 0);
 }
--- dialog_title_box.h
+++ dialog_title_box.h
@@ -1,25 +1,26 @@
 /**
  * Copyright 2020 NotNypical, <https://notnypical.github.io>.
  *
- * This file is part of qTabulator.
+ * This file is part of Tabulator-Qt.
  *
- * qTabulator is free software: you can redistribute it and/or modify
+ * Tabulator-Qt is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * qTabulator is distributed in the hope that it will be useful,
+ * Tabulator-Qt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with qTabulator.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Tabulator-Qt.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef DIALOG_TITLE_BOX_H
 #define DIALOG_TITLE_BOX_H
 
+#include <QHBoxLayout>
 #include <QWidget>
 
 
@@ -29,6 +30,11 @@ class DialogTitleBox : public QWidget
 
 public:
     explicit DialogTitleBox(QWidget *parent = nullptr);
+
+    void setZeroMargins();
+
+private:
+    QHBoxLayout *m_layout;
 };
 
 #endif // DIALOG_TITLE_BOX_H
